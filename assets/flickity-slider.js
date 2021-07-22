aebbad2ba7cb5fff5edb982f35e00ae5820e12ad
bstarr322 <jointstarsoft@gmail.com>
2021-07-22 09:14:24 -0400

    Flickity slider > Update for next, prev button

--- assets/flickity-slider.js
+++ assets/flickity-slider.js
@@ -2,13 +2,18 @@ class FlickityComponent extends HTMLElement {
   constructor() {
     super();
     this.slider = this.querySelector('[data-slider]');
+    this.previousButton = this.querySelector('[data-slider-previous]');
+    this.nextButton = this.querySelector('[data-slider-next]');
+    this.flickityInstance = null;
+
     if (this.slider) {
       this.init();
     }
   }
 
   init() {
-    const flickityInstance = new Flickity( this.slider, {
+    const { prevNextButtons, watchCss } = this.slider.dataset;
+    this.flickityInstance = new Flickity( this.slider, {
       // options
       cellAlign: 'left',
       contain: true,
@@ -16,8 +21,27 @@ class FlickityComponent extends HTMLElement {
       contain: true,
       prevNextButtons: false,
       pageDots: false,
-      watchCSS: true
+      watchCSS: watchCss
     });
+
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    // previous
+    if(this.previousButton != null) {
+      this.previousButton.addEventListener('click', () => {
+        this.flickityInstance.previous(true);
+      });
+    }
+
+    // next
+    if(this.nextButton != null) {
+      this.nextButton.addEventListener('click', () => {
+        this.flickityInstance.next(true);
+      });
+    }
   }
 }
 
